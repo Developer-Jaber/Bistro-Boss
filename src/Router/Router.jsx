7a0a411217d274b54c78d1d829805b104e70cbbc
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2024-12-29 18:41:10 +0400

    Projects setup and overviwe -Done

---
+++ src/Router/Router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout></Layout>,
+        children: [
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path:'/about',
+                element:<h1>about me</h1>
+            }
+        ]
+    }
+])
+
+const Router = () => {
+    return <RouterProvider router={router}></RouterProvider>
+};
+
+export default Router;
